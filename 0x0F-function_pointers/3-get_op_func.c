b149dbffe616212efda227045b0e574acf21971e
Sunnyspp <sunnyspp2013@gmail.com>
2022-10-12 16:11:36 -0700

    Task-3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
 
 /**
-* get_op_func - Selects correct function to perform based on @s
-*
-* @s: The operator
-*
-* Return: The pointer to the operator's function or
-* NULL if not found
-*/
-
+ * get_op_func - function pointer that selects the correct function to perform
+ * the operation asked by the user
+ * @s: the operator given by the user
+ *
+ * Return: pointer to the function that corresponds to the
+ * operator given as a parameter
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -25,13 +22,12 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 
-	while (i < 5)
+	while (ops[i].op)
 	{
-		if (*(ops[i].op) == *s)
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
-
 		i++;
 	}
 
-	return (ops[i].f);
+	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,20 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
 
 /**
-* main - Entry point
-*
-* @argc: length of command line arguments
-*
-* @argv: double pointer to cli arguments
-*
-* Return: 0
-*/
-
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-	int (*func_ptr)(int, int);
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
@@ -22,21 +19,28 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (argv[2][1] != '\0')
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	func_ptr = get_op_func(argv[2]);
+	o = *argv[2];
 
-	if (func_ptr == NULL)
+	if ((o == '/' || o == '%') && arg2 == 0)
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
 
-	printf("%d\n", func_ptr(atoi(argv[1]), atoi(argv[3])));
+	result = func(arg1, arg2);
+
+	printf("%d\n", result);
 
 	return (0);
 }
